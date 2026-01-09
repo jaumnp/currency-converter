dfe0667b532739802c43f1c68b1a41e4c98f3045
Joao Paulo <cudao89@gmail.com>
2026-01-09 19:14:47 -0300

    Refactor currency conversion logic and enhance variable declarations for clarity

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ const USD = 4.87;
 const EUR = 5.32;
 const GBP = 6.08;
 
-const form = document.querySelector('form');
-const amount = document.querySelector('#amount');
-const currency = document.querySelector('#currency');
+const form        = document.querySelector('form');
+const amount      = document.querySelector('#amount');
+const currency    = document.querySelector('#currency');
+const footer      = document.querySelector('main footer');
+const description = document.querySelector('#description');
+const result      = document.querySelector('#result');
+
+const formatCurrencyBRL = (value) => {
+    return Number(value).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+}
 
 const convertCurrency = (amount, currency, symbol) => {
-    console.log(amount, currency, symbol)
+    try {
+
+        const total = formatCurrencyBRL(amount * currency).replace('R$', '');
+        
+        description.textContent = `${symbol}1 = ${formatCurrencyBRL(currency)}`;
+        result.textContent = `${total} reais`;
+
+        footer.classList.add('show-result');
+    } catch(e) {
+        footer.classList.remove('show-result');
+    }
 }
 
 amount.addEventListener('input', () => {
